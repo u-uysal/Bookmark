49e3ee8ce2176679c0c6490dc4e36893b9a985d2
ufuk <ufuk.uysl5@gmail.com>
2020-08-29 19:17:31 +0200

    add bookmark items

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import React from "react";
 
 import "./App.css";
 import Modal from "./components/Modal";
-
+import Bookmark from "./components/Bookmark";
 
 function App() {
   return (
     <>
       <Modal />
+      <Bookmark />
     </>
   );
 }
---
+++ src/components/Bookmark.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Bookmark() {
+  return (
+    <div className="bookmark-container">
+      <div className="bookmark-item">
+        <a href="">Hello</a>
+        <i class="far fa-trash-alt"></i>
+      </div>
+    </div>
+  );
+}
