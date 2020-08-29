44471ace439d60350833ba350d94c37dc50e0505
ufuk <ufuk.uysl5@gmail.com>
2020-08-29 18:20:39 +0200

    add modal and style it

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "./App.css";
+import Modal from "./components/Modal";
+
+
+function App() {
+  return (
+    <>
+      <Modal />
+    </>
+  );
+}
+
+export default App;
